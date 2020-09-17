3f79872dbed8a5654a26b5b2df2f1752a0a2eca3
拉拉醬 <lala.lee.goo@gmail.com>
2020-09-18 07:04:29 +0800

    初步建立 Task Stories

---
+++ src/app/components/task.stories.ts
@@ -0,0 +1,51 @@
+// src/app/components/task.stories.ts
+import { action } from '@storybook/addon-actions';
+import { TaskComponent } from './task.component';
+export default {
+  title: 'Task',
+  excludeStories: /.*Data$/,
+};
+
+export const actionsData = {
+  onPinTask: action('onPinTask'),
+  onArchiveTask: action('onArchiveTask'),
+};
+
+export const taskData = {
+  id: '1',
+  title: 'Test Task',
+  state: 'Task_INBOX',
+  updated_at: new Date(2019, 0, 1, 9, 0),
+};
+export const Default = () => ({
+  component: TaskComponent,
+  props: {
+    task: taskData,
+    onPinTask: actionsData.onPinTask,
+    onArchiveTask: actionsData.onArchiveTask,
+  },
+});
+// pinned task state
+export const Pinned = () => ({
+  component: TaskComponent,
+  props: {
+    task: {
+      ...taskData,
+      state: 'TASK_PINNED',
+    },
+    onPinTask: actionsData.onPinTask,
+    onArchiveTask: actionsData.onArchiveTask,
+  },
+});
+// archived task state
+export const Archived = () => ({
+  component: TaskComponent,
+  props: {
+    task: {
+      ...taskData,
+      state: 'TASK_ARCHIVED',
+    },
+    onPinTask: actionsData.onPinTask,
+    onArchiveTask: actionsData.onArchiveTask,
+  },
+});
