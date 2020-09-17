aa99b3b9ca042de92140bf690b94803cae0638d7
拉拉醬 <lala.lee.goo@gmail.com>
2020-09-18 07:03:59 +0800

    初步建立 Task 元件

---
+++ src/app/components/task.component.ts
@@ -0,0 +1,25 @@
+// src/app/components/task.component.ts
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  template: `
+    <div class="list-item">
+      <input type="text" [value]="task.title" readonly="true" />
+    </div>
+  `,
+})
+export class TaskComponent implements OnInit {
+  title: string;
+  @Input() task: any;
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output() onPinTask: EventEmitter<any> = new EventEmitter();
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output() onArchiveTask: EventEmitter<any> = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit() {}
+}
