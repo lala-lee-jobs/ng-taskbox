72ff025d91b6b1d3a2035fc5cb7a8ae9718590a8
拉拉醬 <lala.lee.goo@gmail.com>
2020-10-01 19:40:03 +0800

    update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <app-inbox-screen></app-inbox-screen>
+  `,
 })
 export class AppComponent {
-  title = 'ng-taskbox';
+  title = 'taskbox';
 }
