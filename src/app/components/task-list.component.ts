5879a6fea788247516f3ec4143aa60a932aa59db
拉拉醬 <lala.lee.goo@gmail.com>
2020-10-01 15:18:15 +0800

    Add task-list.component.ts

---
+++ src/app/components/task-list.component.ts
@@ -0,0 +1,32 @@
+// src/app/components/task-list.component.ts
+
+import { Component, OnInit } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { TasksState, ArchiveTask, PinTask } from '../state/task.state';
+import { Task } from '../models/task.model';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-task-list',
+  template: `
+    <app-pure-task-list
+      [tasks]="tasks$ | async"
+      (onArchiveTask)="archiveTask($event)"
+      (onPinTask)="pinTask($event)"
+    ></app-pure-task-list>
+  `,
+})
+export class TaskListComponent implements OnInit {
+  @Select(TasksState.getAllTasks) tasks$: Observable<Task[]>;
+
+  constructor(private store: Store) {}
+
+  ngOnInit() {}
+  archiveTask(id: string) {
+    this.store.dispatch(new ArchiveTask(id));
+  }
+
+  pinTask(id: string) {
+    this.store.dispatch(new PinTask(id));
+  }
+}
